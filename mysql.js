efcbe64e0f8a964e2193e66ba29a8293a33c5e8b
Agilan <agilan20042003@gmail.com>
2022-11-16 23:50:12 +0530

    finaldamame

--- mysql.js
+++ mysql.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql");
 
 conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'vcdb',
-    port: 3306
+    host: 'mysql5030.site4now.net',
+    user: 'a8fe6b_agiagi1',
+    password: 'Agilan@2003',
+    database: 'db_a8fe6b_agiagi1'
 });
 
 conn.connect((err) => {
